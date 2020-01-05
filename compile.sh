f120866836964d3eb317f9ffecc532a12f1725f9
JasonAn <18086420195@163.com>
2020-01-05 07:48:14 -0800

    [Context] [Author]JasonAn [Date]2020-01-05 [Desp]

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+set -x
+set -e
+
 RootDir=$(dirname $(readlink -f "$0"))
 ProjectDir=$RootDir"/project"
 echo $ProjectDir
@@ -24,15 +27,34 @@ wget -c http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
 tar -zxvf $soundLib
 fi
 
-cd $ProjectDir"/sound/portaudio"
-./configure --with-jack
-make -j2 > /dev/null >&1
+cd $ProjectDir"/sound/portaudio" || exit 1
+./configure --with-jack && make -j2 > /dev/null >&1
 
 
-cd $ProjectDir/build
-cmake $RootDir/src/avs-device-sdk -DGSTREAMER_MEDIA_PLAYER=ON -DPORTAUDIO=ON -DPORTAUDIO_LIB_PATH=$ProjectDir/thirdLib/portaudio/lib/.libs/libportaudio.a -DPORTAUDIO_INCLUDE_DIR=$ProjectDir/thirdLib/portaudio/include  -DCMAKE_BUILD_TYPE=DEBUG
+cd $RootDir"/src" || exit 1
+srcZip="avs-device-sdk-master.zip"
+if [ -f "$srcZip" ]
+then
+cd $RootDir"/src/avs-device-sdk-master" || unzip $srcZip
+else
+cd $RootDir"/src/avs-device-sdk-master" || \
+echo "Please download code source file first."
+fi
 
-make -j2 
+cd $ProjectDir/build || exit 1
+cmake $RootDir/src/avs-device-sdk-master -DGSTREAMER_MEDIA_PLAYER=ON -DPORTAUDIO=ON -DPORTAUDIO_LIB_PATH=$ProjectDir/sound/portaudio/lib/.libs/libportaudio.a -DPORTAUDIO_INCLUDE_DIR=$ProjectDir/sound/portaudio/include  -DCMAKE_BUILD_TYPE=DEBUG
 
+make -j2
+ 
+libFile=$(find $ProjectDir/build -name "*.so")
+#echo $libFile
+if [ "$libFile"!="" ]
+then
+cp $libFile $ProjectDir/lib
+else
+echo "Can't find any so files."
+fi
 
-
+exeFile=$(find $ProjectDir/build -name "SampleApp")
+echo $exeFile
+cp  $exeFile $ProjectDir/bin/SampleApp
